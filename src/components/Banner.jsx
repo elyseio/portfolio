23949dd9c2a368c5e0ce3692ad5e1eafddd4ae0e
ely <elyseio@proton.me>
2023-03-09 23:37:54 +0800

    Added what the home page would look on mobile

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,16 +1,31 @@
+import { useState } from "react";
+
 import "../components/Banner.css";
 import banner from "../assets/evil-morty-matrix.png";
 
+function setMessage() {
+    const message = "We,(glitch)...welcome! You've found the portfolio o..of (glitch)... Weird, anyway, this is an interactive web terminal.";
+    // const [letter, setLetter] = useState("");
+
+    // setLetter(message);
+    // console.log(letter);
+    return message;
+}
+
+
 
 export default function Banner() {
+    const message = setMessage();
+    
     return(
         <div className="wrapper">
             <div className="image-container">
                 <img src={banner} />
             </div>
             <p>
-                We,(glitch)...welcome! You've found the portfolio o..of (glitch)... Weird,
-                anyway, this is an interactive web terminal.
+                {message}
+            </p>
+            <p>
                 For a list of available commands, type <span className="help">'help'</span>.
             </p>
             
